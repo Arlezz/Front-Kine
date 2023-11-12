47c3d8e8493264ace072b2e9be3d93b611c32ae6
Arlezz <antonyrodriguezc.2001@gmail.com>
2023-11-11 23:39:49 -0300

    update responsibe

--- src/components/games/GameCard.jsx
+++ src/components/games/GameCard.jsx
@@ -10,7 +10,9 @@ export function GameCard( {game} ){
     return(
         <a href={game.url} target="_blank" rel="noopener noreferrer" className={styles.gameCardContainer}>  
             <FontAwesomeIcon icon={faGamepad} className={styles.gameIcon}/>
-            <h3 className={styles.gameCardName}>{game.title}</h3>
+            <div className={styles.gameNameContainer}>
+                <span className={styles.gameCardName}>{game.title}</span>
+            </div>
             <div className={styles.gameCardDescription}>
                 {game.description}
             </div>
